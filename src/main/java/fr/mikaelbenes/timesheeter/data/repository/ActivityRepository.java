94dd9faa1da671642b0b22776ea6e7cf402b7c05
Mickael Benes <mickaelbenes@hotmail.fr>
2017-12-05 15:03:54 +0100

    Fixed typo.

--- src/main/java/fr/mikaelbenes/timesheeter/data/repository/ActivityRepository.java
+++ src/main/java/fr/mikaelbenes/timesheeter/data/repository/ActivityRepository.java
@@ -1,11 +1,10 @@
 package fr.mikaelbenes.timesheeter.data.repository;
 
 import fr.mikaelbenes.timesheeter.data.domain.Activity;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Collection;
 
-public interface ActivityRepository extends JpaRepository<Activity, Long> {
+public interface ActivityRepository extends BaseRepository<Activity, Long> {
 
 	Collection<Activity> findByActivityTicket( String activityTicket );
 
